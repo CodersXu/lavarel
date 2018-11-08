ab0a61114edc9f5b19a28c911e815690f445bd54
liuyingjie <handanyingjie@sina.com>
2018-11-08 18:06:35 +0800

    测试钩子

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel555
+                    Laravel66666
                 </div>
 
                 <div class="links">
