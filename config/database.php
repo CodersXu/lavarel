27c2653e4e92028fff3f1ab0df98f922b14ec987
chengxiaoxu <1464648277@qq.com>
2018-11-12 10:34:17 +0800

    修改

--- config/database.php
+++ config/database.php
@@ -43,15 +43,9 @@ return [
             'driver' => 'mysql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-<<<<<<< HEAD
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-=======
-            'database' => env('DB_DATABASE', 'test'),
-            'username' => env('DB_USERNAME', 'rrot'),
-            'password' => env('DB_PASSWORD', 'root'),
->>>>>>> d85777244db54978ba24b4872b80752ee1141acf
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -118,14 +112,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
-        ],
-
-        'cache' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_CACHE_DB', 1),
+            'database' => 0,
         ],
 
     ],
