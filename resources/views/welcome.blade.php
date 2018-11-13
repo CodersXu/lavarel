bd9681999543f3063130aa56a443d96bf02c970e
chengxiaoxu <1464648277@qq.com>
2018-11-13 14:32:21 +0800

    最大的

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel--Github
+                    Laravel--GithubBase
                 </div>
 
                 <div class="links">
