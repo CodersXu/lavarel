f2dff7fedc905f30b2fe90b9cff1a623203cbaea
chengxiaoxu <1464648277@qq.com>
2019-02-15 10:53:36 +0800

    测试

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel--Github程序
+                    Laravel--是的
                 </div>
 
                 <div class="links">
